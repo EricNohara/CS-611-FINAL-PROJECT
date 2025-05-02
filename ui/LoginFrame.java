d244454bfc440b56b347c471fbe28c1c90c311a1
kkuang0 <kkuang@bu.edu>
2025-05-02 01:17:27 -0400

    merge issue

--- ui/LoginFrame.java
+++ ui/LoginFrame.java
@@ -32,11 +32,7 @@ public class LoginFrame extends JFrame {
         
         // Set up the frame
         setTitle("Login");
-<<<<<<< HEAD
-        //setSize(350, 200);
-=======
         setSize(650, 350);
->>>>>>> 98bc80d051447934e2905d344cbd2661ee98bc2c
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         
