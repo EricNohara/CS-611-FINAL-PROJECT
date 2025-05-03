ec09997addfa8861f8052af609c23dc4ca9fabc8
kkuang0 <kkuang@bu.edu>
2025-05-03 17:28:23 -0400

    Initial assignment distribution panel

--- ui/dashboard/TeacherDashboard.java
+++ ui/dashboard/TeacherDashboard.java
@@ -10,6 +10,7 @@ import model.Teacher;
 
 import ui.dashboard.panels.OverviewPanel;
 import ui.dashboard.panels.StudentsPanel;
+import ui.dashboard.panels.TeacherAssignmentStatsPanel;
 import ui.dashboard.panels.CoursesPanel;
 import ui.dashboard.panels.TemplatesPanel;
 import ui.utils.Padding;
@@ -73,6 +74,8 @@ public class TeacherDashboard extends JFrame {
 
         // Students panel
         tabbedPane.addTab("Students", new StudentsPanel(teacher, tabbedPane));
+        
+        tabbedPane.addTab("Assignment Stats", new TeacherAssignmentStatsPanel(teacher));
 
         //refresh
         tabbedPane.addChangeListener(e -> {
